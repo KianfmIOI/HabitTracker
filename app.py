321e393ff940ecfa23bb2184a868a1121883be06
Kian Ghaffarian <kianfmioi@gmail.com>
2026-02-18 13:28:28 +0330

    Initial version of CLI habit tracker with streak logic

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def load_data():
     try:
         with open(DATA_FILE, "r", encoding="utf-8") as f:
             return json.load(f)
-    except FileNotFoundError:
+    except (FileNotFoundError, json.JSONDecodeError):
         return {
             "user": None,
-            "last_date": None,
+            "last_check_in_date": None,
             "habits": {}
         }
 
@@ -42,11 +42,10 @@ def add_habit(data):
         return
 
     today = date.today().isoformat()
-    yesterday = today - timedelta(days=1)
 
     data["habits"][name] = {
-        "start_date": today,
-        "last_date" :yesterday,
+        "creation_date": today,
+        "last_check_in_date" :None,
         "history": [],
         "streak": 0,
         "is_main": False
@@ -55,65 +54,119 @@ def add_habit(data):
     save_data(data)
     print(f"Habit '{name}' added successfully 🔥")
 
-
-def fill_missed_days(data):
-    """Fill missed days with ICE for all habits."""
-    if not data["last_date"]:
+def delete_habit(data):
+    """Delete a habit"""
+    if not data["habits"]:
+        print("no habits to delete.")
         return
+    
+    habits = list(data["habits"].keys())
+    print("\nSelect a habit to delete:")
+    for i,name in enumerate(habits,1):
+        print(f"{i}) {name}")
 
-    last = date.fromisoformat(data["last_date"])
-    today = date.today()
-    missed = (today - last).days - 1
-    if missed > 0:
-        for habit in data["habits"].values():
-            habit["history"].extend([ICE] * missed)
-            habit["streak"] = 0
-
-
+    try:
+        choice = int(input("enter number:").strip())
+        if 1 <= choice <= len(habits):
+            name = habits[choice - 1]
+            del data["habits"][name]
+            save_data(data)
+            print(f"habit {name} deleted")
+        else:
+            print("invalid choice")
+            return
+    except ValueError:
+        print("please enter a valid number")
+        return
 # ----------------------------
 # Check-in logic
 # ----------------------------
-
+    
 def check_in(data):
-    """Daily check-in for all habits."""
+    #data means database
+    """Daily check-in for selected habit."""
     today = date.today().isoformat()
 
+    #if no habits in data
     if not data["habits"]:
         print("No habits yet. Add one first.")
         return
 
-    fill_missed_days(data)
-
     habits = list(data["habits"].keys())
 
-    print("\nWhich habits did you complete today?")
-    for i, name in enumerate(habits, 1):
-        preview = "".join(data["habits"][name]["history"][-7:])
-        print(f"{i}) {name.capitalize():10} {preview}")
+    while True:
+        print("\nWhich habits did you complete today?")
+        print("\n0) Return to main menu")
+        for i, name in enumerate(habits, 1):
+            preview = "".join(data["habits"][name]["history"][-7:])
+            print(f"{i}) {name.title():15} {preview}")
 
-    raw = input("\nEnter numbers (comma-separated): ")
-    completed = {int(x.strip()) for x in raw.split(",") if x.strip().isdigit()}
+        try:
+            choice = int(input("\n select a habit or 0 to exit"))
+            
+            # if user wants to exit
+            if choice == 0:
+                return
+            if 1 <= choice <= len(habits):
+                selected_habit_name = habits[choice-1]
+                selected_habit = data["habits"][selected_habit_name]
 
-    for i, name in enumerate(habits, 1):
-        habit = data["habits"][name]
-        if i in completed:
-            habit["history"].append(FIRE)
-            habit["streak"] += 1
-        else:
-            habit["history"].append(ICE)
-            habit["streak"] = 0
+                # if we've checked in today
+                if selected_habit["last_check_in_date"] == today:
+                    print("You already checked in today 🔥")
+                    continue
+                # if new habit
+                elif selected_habit["last_check_in_date"] is None:
+                    selected_habit["last_check_in_date"] = today
+                    selected_habit["history"].append(FIRE)
+                    selected_habit["streak"] += 1
+                    print(f"\n habit {selected_habit_name} checked in{FIRE}")
 
-    data["last_date"] = today
-    print("\nCheck-in saved 🔥")
+                # if old habit
+                else:
+                    fill_missed_days(selected_habit)
+                    break_streak(selected_habit)
+                    selected_habit["history"].append(FIRE)
+                    selected_habit["streak"] += 1
+                    selected_habit["last_check_in_date"] = today
+                    print(f"\n habit {selected_habit_name} checked in{FIRE}")
 
+                preview = selected_habit["history"][-7:]
+                print(f"\n{selected_habit_name} streak: {selected_habit['streak']} days")
+                print(preview)
+                save_data(data)
 
+            
+            # not a number in the list
+            else:
+                print(f"please enter a number between 0 and {len(habits)}")
+
+        except ValueError:
+            print("Invalid input, please enter a valid number")
+
+def fill_missed_days(habit):
+    """Fill missed days with ICE for selected habits."""
+
+    last = date.fromisoformat(habit["last_check_in_date"])
+    today = date.today()
+    
+    missed = (today - last).days - 1
+    if missed > 0:
+        habit["history"].extend([ICE] * missed)
+
+def break_streak(habit):
+    if habit["last_check_in_date"] is None:
+        return
+    if habit["history"][-1] == ICE:
+        habit["streak"] = 0
+      
 # ----------------------------
 # Display
 # ----------------------------
 
 def show_status(data):
     if not data["habits"]:
-        print("no habits history ")
+        print("no habits available ")
         return
     """Show habit streaks and last 7 days."""
     print("\nYour habits status:\n")
@@ -129,7 +182,7 @@ def show_status(data):
         else:
             status = "🧊"
 
-        print(f"{name.capitalize():10} {last_7:10}  Streak: {streak} {status}")
+        print(f"{name.title():15} {last_7:10}  Streak: {streak} {status}")
 
 
 # ----------------------------
@@ -146,8 +199,9 @@ def main():
         print("\nChoose an option:")
         print("0) Add a new habit")
         print("1) Check in today")
-        print("2) Show status")
-        print("3) Exit")
+        print("2) delete a habit")
+        print("3) Show status")
+        print("4) Exit")
 
         choice = input("Your choice: ").strip()
 
@@ -156,8 +210,10 @@ def main():
         elif choice == "1":
             check_in(data)
         elif choice == "2":
-            show_status(data)
+            delete_habit(data)
         elif choice == "3":
+            show_status(data)
+        elif choice == "4":
             save_data(data)
             print("See you tomorrow 🔥")
             break
